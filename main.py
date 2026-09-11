7925f0d5e7fa2a0e17ca75e35ebc2ce091b1fbfa
daurenovvva <pj6yzb25bm@privaterelay.appleid.com>
2026-09-11 16:54:18 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Python Project")
+
+name = input("Атыңызды енгізіңіз: ")
+age = int(input("Жасыңызды енгізіңіз: "))
+
+print("Сәлем,", name + "!")
+print("Келесі жылы жасыңыз", age + 1, "болады.")
